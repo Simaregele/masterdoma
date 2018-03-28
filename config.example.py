25c329b5f453453db9b688d419c959d4ec7a4992
Simaregele <mrsimaregele@gmail.com>
2018-03-29 00:51:29 +0500

    add some example

---
+++ config.example.py
@@ -0,0 +1,17 @@
+
+#for telegram
+token = 'some token 1'
+token_dis = 'some token 2'
+
+#server
+server_host = 'server-huerver.er'
+server_ip = '000.000.00.00'
+
+#certificate_ssl
+cert_path = '/etc/letsencrypt/live/server-huerver.er/fullchain.pem'
+key_path = '/etc/letsencrypt/live/server-huerver.er/privkey.pem'
+
+#for VK
+vk_gr_token = 'nice token'
+vk_login = 'name'
+vk_pass = 'pass'
